5f60ecb238cee929b370d39f7df2208705173213
Msgyankee <mgorman@stevens.edu>
2017-12-04 22:58:55 -0500

    Added JS to About

---
+++ js/about.js
@@ -0,0 +1,47 @@
+window.addEventListener("load",function(){
+    
+    var maddie = document.getElementById("maddie");
+    var maddietext = document.getElementById("maddietext")
+    maddie.addEventListener("mouseover",function(){
+        maddietext.style.color = "red";
+    });
+    maddie.addEventListener("mouseout",function(){
+       maddietext.style.color = "black"; 
+    });
+    
+    var matt = document.getElementById("matt");
+    var matttext = document.getElementById("matttext")
+    matt.addEventListener("mouseover",function(){
+        matttext.style.color = "red";
+    });
+    matt.addEventListener("mouseout",function(){
+       matttext.style.color = "black"; 
+    });
+    
+    var lehai = document.getElementById("lehai");
+    var lehaitext = document.getElementById("lehaitext")
+    lehai.addEventListener("mouseover",function(){
+        lehaitext.style.color = "red";
+    });
+    lehai.addEventListener("mouseout",function(){
+       lehaitext.style.color = "black"; 
+    });
+
+    var hunter = document.getElementById("hunter");
+    var huntertext = document.getElementById("huntertext")
+    hunter.addEventListener("mouseover",function(){
+        huntertext.style.color = "red";
+    });
+    hunter.addEventListener("mouseout",function(){
+       huntertext.style.color = "black"; 
+    });
+    
+    var justin = document.getElementById("justin");
+    var justintext = document.getElementById("justintext")
+    justin.addEventListener("mouseover",function(){
+        justintext.style.color = "red";
+    });
+    justin.addEventListener("mouseout",function(){
+       justintext.style.color = "black"; 
+    });
+});
